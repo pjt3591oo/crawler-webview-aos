5367134363613c9e03bf90691b7592566b181c1e
pjt3591oo <pjt3591oo@gmail.com>
2023-12-10 10:53:54 +0900

    웹뷰 화면 구성

--- app/src/main/java/com/example/crawler_webview_aos/MainActivity.java
+++ app/src/main/java/com/example/crawler_webview_aos/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.crawler_webview_aos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView myWebView = findViewById(R.id.webview);
+
+        WebSettings webSettings = myWebView.getSettings();
+        // Enable JavaScript in the WebView
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        webSettings.setSupportMultipleWindows(true);
+
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.setWebChromeClient(new WebChromeClient());
+        myWebView.loadUrl("https://www.google.com");
     }
 }
